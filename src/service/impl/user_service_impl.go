743f40865de3ee9bfab44c02bda6ebe8dd97ab97
M. Aji Perdana <ajicooljazz38@gmail.com>
2025-01-12 12:46:22 +0700

    feat: create role service ⚗🧱

--- src/service/impl/user_service_impl.go
+++ src/service/impl/user_service_impl.go
@@ -3,14 +3,11 @@ package impl
 import (
 	"errors"
 	"fmt"
-	"log"
 	"res-gin/src/dto"
 	"res-gin/src/model"
 	"res-gin/src/service"
-	"time"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -33,11 +30,13 @@ func NewUserService(db *gorm.DB) service.UserService {
 
 func (s *UserServiceImpl) CreateUser(userDto *dto.CreateUserDTO) (*model.Users, error) {
 	err := s.validate.Struct(userDto)
+
 	if err != nil {
 		return nil, err
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), bcrypt.DefaultCost)
+
 	if err != nil {
 		return nil, err
 	}
@@ -54,51 +53,39 @@ func (s *UserServiceImpl) CreateUser(userDto *dto.CreateUserDTO) (*model.Users, 
 		}
 	}()
 
-	userID := uuid.New().String()
-	userQuery := `INSERT INTO "users" ("id", "email", "username", "password", "created_at", "updated_at") 
-								VALUES (?, ?, ?, ?, ?, ?) RETURNING "id"`
-	if err := tx.Raw(userQuery, userID, userDto.Email, userDto.Username, string(hashedPassword), time.Now(), time.Now()).Scan(&userID).Error; err != nil {
+	user := &model.Users{
+		Email:    userDto.Email,
+		Username: userDto.Username,
+		Password: string(hashedPassword),
+	}
+
+	if err := tx.Create(&user).Error; err != nil {
 		tx.Rollback()
 		return nil, err
 	}
-	log.Println("User inserted successfully, ID:", userID)
 
-	var roleID string
-	roleQuery := `SELECT "id" FROM "roles" WHERE "role" = ? LIMIT 1`
-	err = tx.Raw(roleQuery, userDto.Role).Scan(&roleID).Error
+	role, err := s.roleService.GetOrSaveRole(userDto.Role)
 
-	if err != nil || roleID == "" {
-		roleID = uuid.New().String()
-		insertRoleQuery := `INSERT INTO "roles" ("id", "role", "created_at", "updated_at") 
-												VALUES (?, ?, ?, ?) RETURNING "id"`
-		if err := tx.Raw(insertRoleQuery, roleID, userDto.Role, time.Now(), time.Now()).Scan(&roleID).Error; err != nil {
-			tx.Rollback()
-			return nil, err
-		}
-		log.Println("Role inserted successfully, ID:", roleID)
-	} else {
-		log.Println("Role already exists, ID:", roleID)
-	}
+	fmt.Print(role)
 
-	userRoleQuery := `INSERT INTO "user_roles" ("users_id", "role_id") 
-										VALUES (?, ?) ON CONFLICT DO NOTHING`
-	if err := tx.Raw(userRoleQuery, userID, roleID).Error; err != nil {
+	if err != nil {
 		tx.Rollback()
 		return nil, err
 	}
-	log.Println("User-role relationship inserted successfully")
+
+	userRolesQuery := `INSERT INTO user_roles (users_id, role_id)
+									 VALUES ($1, $2) ON CONFLICT (users_id, role_id) DO NOTHING`
+
+	if err := tx.Exec(userRolesQuery, user.ID, role.ID).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
 
 	if err := tx.Commit().Error; err != nil {
 		return nil, err
 	}
 
-	return &model.Users{
-		ID:       userID,
-		Email:    userDto.Email,
-		Username: userDto.Username,
-		Password: string(hashedPassword),
-		Roles:    []model.Role{{ID: roleID, Role: userDto.Role}},
-	}, nil
+	return user, nil
 }
 
 func (s *UserServiceImpl) GetAllUsers() ([]model.Users, error) {
